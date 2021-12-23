e4a0b80148c018285f43e7675e30c59bf1cd3131
B6X35 <d.b.palamarchuk@gmail.com>
2021-12-23 15:30:53 +0200

    sdaf

--- src/shared/service/api.js
+++ src/shared/service/api.js
@@ -12,19 +12,37 @@ const getPopular = () => {
 };
 
 const getSearch = (name) => {
-    return instnce.get(`/search/movie?api_key=${API_KEY}&query=${name}&language=en-US&page=1&include_adult=false`)
-};
+    return instnce.get(`/search/movie?api_key=${API_KEY}`, {
+    params: {
+        query: name,
+        language: 'en-US',
+        page: 1,
+        include_adult: false,
+    }    
+})};
 
 const getDetails = (id) => {
-    return instnce.get(`/movie/${id}?api_key=${API_KEY}&language=en-US`)
+    return instnce.get(`/movie/${id}?api_key=${API_KEY}`, {
+        params: {
+            language: 'en-US'
+        }
+    })
 }
 
 const getCredits = (id) => {
-    return instnce.get(`/movie/${id}/credits?api_key=${API_KEY}&language=en-US`)
+    return instnce.get(`/movie/${id}/credits?api_key=${API_KEY}`, {
+        params: {
+            language: 'en-US'
+        }
+    })
 }
 
 const getReviews = (id) => {
-    return instnce.get(`/movie/${id}/reviews?api_key=${API_KEY}&language=en-US`)
+    return instnce.get(`/movie/${id}/reviews?api_key=${API_KEY}`, {
+        params: {
+            language: 'en-US'
+        }
+    })
 }
 
 export const ApiService = {
